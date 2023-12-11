c5df2bca21a7d7d56585629bf4c21458702a3c18
JillianDreemur <marifalb2004@gmail.com>
2023-12-11 14:42:49 -0300

    mais codigos

--- br/estoque/src/Model/Cliente.java
+++ br/estoque/src/Model/Cliente.java
@@ -1,11 +1,40 @@
 package br.estoque.src.Model;
+import java.util.Scanner;
 
 public class Cliente {
-    public Carrinho[] carrinho = new Carrinho[1];
-    public double limiteGasto;
+    public Carrinho[] carrinho;
+    public double limiteDeGasto;
     public int conta = 0;
 
-    public Cliente(double limiteDeGasto){
-        this.limiteGasto = limiteDeGasto;
+    public Cliente(double limiteDeGasto, int limiteCarrinho){
+        this.limiteDeGasto = limiteDeGasto;
+        this.carrinho = new Carrinho[limiteCarrinho];
+    }
+
+    public void verificarConta(){
+        for(int i = 0; i < this.carrinho[0].produtosNoCarrinho.length; i++){
+            conta += carrinho[0].produtosNoCarrinho[i].qntEstoque;
+        }
+        System.out.println("Gostaria de finalizar a compra " +
+                "agora e fechar a conta? (1 - Sim / 2 - Não)");
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            int escolha = scanner.nextInt();
+            if(conta == 1){
+                System.out.println("Obrigada pela preferência e volte " +
+                        "sempre!");
+                conta = 0;
+                for(int i = 0; i < carrinho[0].
+                        produtosNoCarrinho.length; i++){
+                    carrinho[0].produtosNoCarrinho[i] = null;
+                }
+                break;
+            } else if (conta == 2){
+                System.out.println("Boas compras!");
+                break;
+            } else {
+                System.out.println("Escolha uma opção válida.");
+            }
+        }
     }
 }
